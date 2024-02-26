43babc04185ded45904650f2ef1350fa7908291c
Evans <evanstats808@gmail.com>
2024-02-26 18:14:22 +0300

    finishing smart contract and started on frontend

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -14,8 +14,22 @@ contract Dappazon {
         uint256 _stock;
 
     }
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+    mapping(uint256 => Item) public items;
+    mapping (address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
 
     
+    event List (string name, uint256 cost, uint256 quantity);
+    //event Buy (address buyer, uint256 orderId, uint256 ItemId);
+    
+    modifier onlyDeployer {
+        require(msg.sender == owner, "only owner can call this function");
+        _;
+    }
     
     constructor() {
         owner= msg.sender;
@@ -30,9 +44,40 @@ contract Dappazon {
         uint256 _rating,
         uint256 _stock
 
-    ) public {
+    ) public onlyDeployer {
+        //create item struct
         Item memory item = Item(_id, _name, _category,_image, _cost, _rating, _stock); 
+        //Save Item Struct to blockchain
+        items[_id] = item;
+        //emit event
 
-        
+        emit List(_name, _cost, _stock);
+
+    }
+
+    function buy(uint256 _id) public payable {
+        //fetch item
+        Item memory item = items[_id];
+
+        require(msg.value >= item._cost);
+        require(item._stock > 0);
+        //create order
+        Order memory order = Order(block.timestamp, item);
+        //save order to blockchain
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        //subtract stock
+        items[_id]._stock = item._stock - 1;
+        //emit event
+        //event Buy (msg.sender, orderCount[msg.sender], item.id);
+
+
+    }
+
+    //withdraw funds
+    function withdraw() public onlyDeployer() {
+        (bool success,) = owner.call{value: address(this).balance}("");
+        require(success);
     }
 }
--- src/App.js
+++ src/App.js
@@ -12,11 +12,45 @@ import Dappazon from './abis/Dappazon.json'
 // Config
 import config from './config.json'
 
+
+
 function App() {
+  const [account, setAccount] = useState(null)
+  const [provider, Setprovider] = useState(null)
+  const [dappazon, setDappazon] = useState(null)
+
+  const loadBlockchain = async () => {
+    //Connect to Blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    Setprovider(provider)
+    console.log('Loading......')
+
+    const network = await provider.getNetwork()
+    console.log(network)
+
+    //Connect to smart contracts (Create JS Versions)
+    const dappazon = new ethers.Contract(
+      config[network.chainId].dappazon.address,
+      Dappazon,
+      provider
+    )
+    setDappazon(dappazon)
+      const items = []
+      for (var i =0; i < 9; i++) {
+        const item = await dappazon.items(i + 1)
+        items.push(item)
+      }
+      
+
+  }
+
+  useEffect(()=> {
+    loadBlockchain()
+  },[])
 
   return (
     <div>
-
+      <Navigation account={account} setAccount={setAccount} />
       <h2>Welcome to Dappazon</h2>
 
     </div>
